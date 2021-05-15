af74ab70c0fad3139b62a4c6a74b8302499aa921
jingling688 <84231039+jingling688@users.noreply.github.com>
2021-05-15 20:35:06 +0800

    这仅仅是一个测试

---
+++ 66.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+     int i,j;
+     int x=0;
+     for(i=0;i<3;i++)
+     {
+         for (;j<=2;j++)
+         {
+             x++;
+         }
+     }
+printf ("%d",x);
+}
+
